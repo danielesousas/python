edf994fe0df8cb98015ae8a7ff32b5efdefa5bbd
DanieleSousa <145569612+danielesousas@users.noreply.github.com>
2024-01-17 11:38:36 -0300

    dunder

---
+++ 39dunder.py
@@ -0,0 +1,31 @@
+"""
+Objetos Dunder
+Dunder - Duplo Underscore - Duplo Underline
+Atributos Mágicos - variáveis
+Métodos Mágicos - funções
+
+__init__ - Python 2.x
+__name__ - Nome do Arquivo
+
+__main__ - Caminho do arquivo
+__file__ - 
+__doc__ - Documentação de forma legível, deve ser feito dentro de aspas triplas
+
+"""
+
+import random
+
+if __name__ == '__main__':
+    print('ola, mundo primo')
+
+
+"""
+import primo
+__doc__
+primo.__doc__
+print(primo.__doc__)  -> torna a acão mais legivel
+
+import random
+random.__doc__
+
+"""
---
+++ 40modulo_os.py
@@ -0,0 +1,6 @@
+import os
+
+print(os.name)#indica qual sistema operacional estou usando
+
+
+print(os.path.exists('texto.py'))#verifica se arquivo existe 
